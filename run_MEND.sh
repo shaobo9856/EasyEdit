eb8e4e4377fa763b6292658fe6c5714354128ef0
shaobo9856 <26827545+shaobo9856@users.noreply.github.com>
2024-07-17 11:39:09 +0200

    hpparams for mend

--- run_MEND.sh
+++ run_MEND.sh
@@ -2,11 +2,11 @@
 
 LANGS=("es" "vi" "ru" "zh-cn")
 DATAS=("MzsRE/mzsre_test_duplicate_en" "MCounterFact/mcounterfact_test_en" "WikiFactDiff/wfd_test_en")
-CUDA=1
+CUDA=0
 
 for DATA in "${DATAS[@]}";do
     for LANG in "${LANGS[@]}";do
         echo "currently processing language: $LANG with data: $DATA"
-        CUDA_VISIBLE_DEVICES=$CUDA python run_zsre_llama2.py --lang1 en --lang2 $LANG --editing_method KN --hparams_dir ./hparams/KN/llama-7b.yaml  --data_dir $DATA 
+        CUDA_VISIBLE_DEVICES=$CUDA python run_zsre_llama2.py --lang1 en --lang2 $LANG --editing_method MEND --hparams_dir ./hparams/MEND/llama-7b.yaml  --data_dir $DATA 
     done
 done
